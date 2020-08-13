c176d421ba9753c95cb2ee8f3dbf8be9627259d5
datquocngo <shaco14091999@gmail.com>
2020-08-13 17:06:52 +0700

    register done

--- src/main/java/com/example/demo/service/user/UserService.java
+++ src/main/java/com/example/demo/service/user/UserService.java
@@ -4,6 +4,8 @@ import com.example.demo.model.user.Passenger;
 import com.example.demo.model.user.User;
 import com.example.demo.repository.user.PassengerRepository;
 import com.example.demo.repository.user.UserRepository;
+import com.example.demo.service.location.GeoPointService;
+import com.example.demo.service.location.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,10 @@ public class UserService {
     private PassengerRepository passengerRepository;
     @Autowired
     private PasswordEncoder bcryptEncoder;
+    @Autowired
+    private GeoPointService geoPointService;
+    @Autowired
+    private LocationService locationService;
 
     public List<User> findAll() {
         return userRepository.findAll();
@@ -28,7 +34,10 @@ public class UserService {
     }
 
     public void createUser(User user) {
-        user.setPassword(bcryptEncoder.encode(user.getPassword()));
+        locationService.updateLocation(user.getHomeAddress().getLocation());
+        locationService.updateLocation(user.getOfficeAddress().getLocation());
+        geoPointService.updateGeoPoint(user.getHomeAddress());
+        geoPointService.updateGeoPoint(user.getOfficeAddress());
         userRepository.save(user);
         passengerRepository.save(new Passenger(user));
     }
